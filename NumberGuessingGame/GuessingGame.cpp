32483a1091b9dfaf2d822e64212e0f6c60dd0f12
spacegeek224 <marisusisme@gmail.com>
2015-07-21 16:02:38 -0400

    Added int main() and setGuess()

--- NumberGuessingGame/GuessingGame.cpp
+++ NumberGuessingGame/GuessingGame.cpp
@@ -5,7 +5,10 @@
 
 using namespace System;
 using namespace System::Windows::Forms;
+using namespace std; //if problems occur, then remove using namespace std
 
+int num;
+int guess[7];
 
 [STAThread]
 void Main(array<String^>^ args)
@@ -15,4 +18,14 @@ void Main(array<String^>^ args)
 
 	NumberGuessingGame::GuessingGame form;
 	Application::Run(%form);
+}
+int main() {
+	srand((unsigned int)time(0));
+	num = rand() % 100;
+	for ( int i=0; i<8; i++ ) {
+		
+	}
+}
+void setGuess(int numguess, int guess) {
+
 }
